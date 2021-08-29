23450543f1936a313530139ae16b2e4ce89242d9
lixiaofen1 <1025209986@qq.com>
2021-08-29 12:50:43 +0800

    引用修改
    添加引用修改

--- 引用.cpp
+++ 引用.cpp
@@ -5,7 +5,7 @@
 
 #include <iostream>
 using namespace std;
-void swap(int a,int b)
+void swap1(int a,int b)
 {
     int temp;
     temp = a;
@@ -15,7 +15,7 @@ void swap(int a,int b)
 int main()
 {
     int i = 3,j = 5;
-    swap(i,j);
+    swap1(i,j);
     cout<<i<<","<<j<<endl;
     return 0;
 }
@@ -26,7 +26,7 @@ int main()
  * **************/
 #include<iostream>
 using namespace std;
-void swap(int *a,int *b)
+void swap2(int *a,int *b)
 {
     int temp = *a;
     *a = *b;
@@ -37,7 +37,7 @@ int main()
 {
     //int *j = 3;*i = 5;指针可以这样赋值吗
     int i = 3,j = 5;
-    swap(&i,&j);
+    swap2(&i,&j);
     cout << i <<","<<j<<endl;
     return 0;
 }
@@ -48,7 +48,7 @@ int main()
  *  **************/
 #include <iostream>
 using namespace std;
-swap(int &a,int &b)
+void swap3(int &a,int &b)
 {
     int temp = a;
     a = b;
@@ -58,7 +58,7 @@ swap(int &a,int &b)
 int main()
 {
     int i = 3,j = 5;
-    swap(i,j);
+    swap3(i,j);
     return 0;
 }
 
@@ -100,6 +100,36 @@ int text(int *p)
  * 3、引用就相当于变量本身
  *      int &b = a;(b此时相当于a)
  *      int *p  =&b;（相当于 int *p = &a;）
- * 
  *      
+ *      建立指针变量的引用
+ *      int i = 5;
+ *      int *p = &i;
+ *      int* &pt = p;
+ * 
+ * 4、使用const对引用加以限定，不允许改变该引用的值
+ *      int i = 5;
+ *      const int &a = i;
+ *      a = 3;(不合法)
+ *      i= 3；（合法）
+ * 
+ * 5、可以用常量或表达式对引用进行初始化，但此时必须用const作声明：
+ *      int i = 5;
+ *      const &a = i + 3;
+ *      此时编译系统这样处理：生成一个临时变量，用来存放该表达式的值，引用是该临时变量的别名，系统
+ *      将“const &a = i +3;”转换为：
+ *      int temp = i+ 3;
+ *      const int &a = temp;
+ * 
+ *      使用不同的变量初始化引用（要求赋值兼容的类型）
+ *      double d = 3.1415926;
+ *      const int &a = d;   //使用d初始化a
+ *      
+ *      编译系统将const int &a = d;转化为
+ *      int temp = d;   //先将double类型变量d转换为int型，存放在temp中
+ *      const int &a = temp;    //temp和a同类型
+ *      此时如果输出引用a的值，将是3而不是3.1415926。因为从根本上说，只能对变量建立引用。
+ *      如果上面声明时不同const，则会发生错误。如果不加const，则引用a的值则可以更改，导致临时变量temp的
+ *      值同时发生更改，而变量d的值并没有变。会存在二义性。
+ * 
+ *      加上引用const就像时主从的关系，值的变化只能从主变量向下流，不能从引用向上流。有一个方向
  * *******/
